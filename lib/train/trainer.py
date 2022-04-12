a351c8201852a86250fa609d92ebebbc408d15d0
Vinit Awale <awale.vinit@gmail.com>
2022-04-12 23:51:19 +0530

    update trainer.py

--- lib/train/trainer.py
+++ lib/train/trainer.py
@@ -27,7 +27,8 @@ class Trainer:
         self.writer = TensorboardWriter(args)
 
         self.save_frequency = 10
-        self.terminal_show_freq = self.args.terminal_show_freq
+        # self.terminal_show_freq = self.args.terminal_show_freq
+        self.terminal_show_freq = 10
         self.start_epoch = 1
 
     def training(self):
